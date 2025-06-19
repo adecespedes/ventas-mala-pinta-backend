fe048213f4c0c4e3544283fb8b9209116bada3fb
Alejandro <adecespedes1990@gmail.com>
2025-06-19 11:35:03 -0400

    enableCors

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.enableCors({
-    origin: 'http://217.160.88.116:8081', // o '*' para permitir todo (menos seguro)
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  app.enableCors();
 
   await app.listen(process.env.PORT ?? 3000);
 }
