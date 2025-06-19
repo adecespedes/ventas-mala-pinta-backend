5f1b1f7feb2ce48677f1d964a733af0de213d70d
Alejandro <adecespedes1990@gmail.com>
2025-06-19 10:33:15 -0400

    updating dependencies

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,14 +6,16 @@ import { Detalle } from './modules/detalle/detalle.entity';
 import { Producto } from './modules/productos/producto.entity';
 import { DetalleVenta } from './modules/detalleVenta/detalle-venta.entity';
 
-export const AppDataSource = new DataSource({
+const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'adecespedes',
-  password: '',
-  database: 'ventas',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: Number(process.env.POSTGRES_PORT) || 5432,
+  username: process.env.POSTGRES_USER || 'adecespedes',
+  password: process.env.POSTGRES_PASSWORD || 'postgres',
+  database: process.env.POSTGRES_DB || 'ventas',
   entities: [Paca, Venta, Tipo, Detalle, Producto, DetalleVenta],
   migrations: ['src/migration/*.ts'],
   synchronize: false, // En migraciones debe ser false
 });
+
+export default AppDataSource;
